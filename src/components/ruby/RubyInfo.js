98b27630b06877436409bb84a1d2943c41fa8cad
Henry Doan <henrydoan96@gmail.com>
2018-12-26 18:47:51 -0700

    transfer content to ruby

--- src/components/ruby/RubyInfo.js
+++ src/components/ruby/RubyInfo.js
@@ -23,11 +23,56 @@ class RubyInfo extends Component {
             <div id="introduction" className="section scrollspy">
               <p>Ruby</p>
             </div>
+
+            <div id="history" className="section scrollspy">
+              <p>
+                {/* TODO */}
+              </p>
+            </div>
+
+            <div id="files" className="section scrollspy">
+              <h5>How to Make a Ruby File</h5>
+              <p>
+                Each programming language has its own file extension.
+                Ruby files end in ".rb"
+                Here are some examples:
+              </p>
+              <div className="code-box">
+                <code>
+                  main.rb<br />
+                  controller.rb<br />
+                  notes.rb<br />
+                </code>
+              </div>
+            </div>
+
+            <div id="data-types" className="section scrollspy">
+              <p>
+                {/* TODO */}
+              </p>
+            </div>
+
+            <div id="operators" className="section scrollspy">
+              <p>
+                {/* TODO */}
+              </p>
+            </div>
+
+            <div id="comparators" className="section scrollspy">
+              <p>
+                {/* TODO */}
+              </p>
+            </div>
           </div>
 
           <div className="col hide-on-small-only m3 l2">
             <ul className="section table-of-contents">
               <li><a href="#introduction">Introduction</a></li>
+              <li><a href="#history">History</a></li>
+              <li><a href="#files">Ruby Files</a></li>
+              <li><a href="#data-types">Data Types</a></li>
+              <li><a href="#operators">Operators</a></li>
+              <li><a href="#comparators">Comparators</a></li>
             </ul>
           </div>
         </div>
